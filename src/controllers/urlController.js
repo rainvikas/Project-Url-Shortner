584b6655c0806f75700ce7c8a29e119805600e00
rainvikas <99880869+rainvikas@users.noreply.github.com>
2022-04-09 23:53:51 +0530

    Update urlController.js

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -2,7 +2,7 @@ const UrlModel = require("../Models/urlModel");
 const validUrl = require('valid-url')
 const shortId = require('shortid')
 const redis = require('redis')
-const util = require("util");
+const {promisify} = require("util");
 
 const redisClient = redis.createClient(
     19837,
@@ -17,8 +17,8 @@ redisClient.on("connect", async function () {
     console.log("Connected to Redis..");
 });
 
-const SET_ASYNC = util.promisify(redisClient.SET).bind(redisClient);
-const GET_ASYNC = util.promisify(redisClient.GET).bind(redisClient);
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 const isValid = function (value) {
     if (typeof value === 'undefined' || value === null) return false
@@ -44,14 +44,16 @@ const createUrl = async function (req, res) {
         if (!(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g.test(longUrl))) {
             return res.status(400).send({ status: false, msg: "longUrl is not a valid url" })
         }
-        let urlDetails = await GET_ASYNC(`${longUrl}`)
-        if (urlDetails) {
-            res.status(200).send({ status: true, msg: "details fetched successfully", data: urlDetails })
-        }
+        
         let longUrlDetails = await UrlModel.findOne({ longUrl }).select({createdAt:0,updatedAt:0,__v:0,_id:0})
         if (longUrlDetails) {
             await SET_ASYNC(`${longUrl}`, JSON.stringify(longUrlDetails),"EX",20)
             return res.status(200).send({ status: true, msg: "data fetched successfully", data: longUrlDetails })
+        }
+        let urlDetails = await GET_ASYNC(`${longUrl}`)
+        let changeToObject = JSON.parse(urlDetails)
+        if (changeToObject) {
+            return res.status(200).send({ status: true, msg: "details fetched successfully", data: changeToObject })
         }
         else {
             let urlCode = shortId.generate()
@@ -61,10 +63,6 @@ const createUrl = async function (req, res) {
             }
             let baseUrl = 'http://localhost:3000'
             let shortUrl = baseUrl + '/' + urlCode
-            // let isShortUrlAlreadyUsed = await urlModel.findOne({ shortUrl })
-            // if (isShortUrlAlreadyUsed) {
-            //     return res.status(500).send({ status: false, msg: "server error" })
-            // }
             let urlToBeCreated = { urlCode: urlCode, longUrl, shortUrl: shortUrl }
             let createNewUrl = await UrlModel.create(urlToBeCreated)
             return res.status(201).send({ status: true, msg: "url created successfully", data: createNewUrl })
